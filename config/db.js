1b76becf669ec8768cf35243a4e04cc9614a1146
mai <125476096+mai3331@users.noreply.github.com>
2024-10-21 18:01:16 +0300

    l

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/MovieApp');
+        await mongoose.connect('mongodb+srv://mai6228731:4p8hDaISmXDQODhH@moviematch.qcipk.mongodb.net/');
         console.log('MongoDB connected successfully!');
     } catch (error) {
         console.error('MongoDB connection error', error.message);
